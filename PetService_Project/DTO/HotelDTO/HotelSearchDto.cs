f9678ded51fc06a763c89117b19b0a2a070dbcb3
Angel <gcobs064046@yahoo.com.tw>
2025-05-13 16:50:27 +0800

    建立HotelController、HotelDto資料夾

---
+++ PetService_Project/DTO/HotelDTO/HotelSearchDto.cs
@@ -0,0 +1,11 @@
+﻿namespace PetService_Project_Api.DTO.HotelDTO
+{
+    public class HotelSearchDto
+    {
+        public DateTime? CheckInDate { get; set; }
+        public DateTime? CheckOutDate { get; set; }
+        public int PetCount { get; set; }
+        public List<string>? Amenity { get; set; }  // ex: 設施
+        public List<string>? Service { get; set; }   // ex: 服務
+    }
+}
