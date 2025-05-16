b8f55b9e6ef879027b9fbfe474ad896dce597de3
Ryan <xray910422@gmail.com>
2025-05-16 14:09:54 +0800

    重新製作DTO,EmployeesController

---
+++ PetService_Project/DTO/WalkDTOs/EmployeeDetailResponseDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace PetService_Project_Api.DTO.WalkDTOs
+{
+    public class EmployeeDetailResponseDTO
+    {
+        public string Name { get; set; }
+        public string District { get; set; }
+        public int Price { get; set; }
+        public List<string> PetTypes { get; set; }
+        public List<string> PetSizes { get; set; }
+        public int Distance { get; set; }
+        public string DescriptionShort { get; set; }
+        public string Description { get; set; }
+        public List<string> Photos { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
